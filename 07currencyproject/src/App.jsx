24396e693d404446731343eb3fa964de3930fff7
aditya <fordevelopmentusecase@gmail.com>
2025-05-26 00:35:23 +0530

    initialize React project with Vite, Tailwind CSS, and ESLint configuration; add basic components and hooks for currency conversion

---
+++ 07currencyproject/src/App.jsx
@@ -0,0 +1,13 @@
+import Input from "./components/Input"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Currency Converter</h1>
+      <h2 className="text-2xl">Hello</h2>
+      <Input />
+    </>
+  )
+}
+
+export default App
---
+++ 07currencyproject/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Input() {
+  return (
+    <div className="w-fit h-auto flex bg-amber-300">
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Input
